97f7a6d137d3cd537900b4946a30a0302fa14b24
bmh10 <bmhomer13@gmail.com>
2017-02-28 20:13:16 +0000

    Compiling again

--- Main.hs
+++ Main.hs
@@ -25,14 +25,14 @@ data KeyPress  = South | East | West | None deriving (Eq, Show)
 data Tile = Tile
   {
     pos :: (Float, Float)
-  }
+  } deriving Show
 
 data Block = Block
   {
     blockType :: BlockType,
     rotation  :: Float,
     tiles     :: [Tile]
-  }
+  } deriving Show
 
 data TetrisGame = Game
   {
@@ -47,16 +47,19 @@ data TetrisGame = Game
 
 initGame = do
   stdGen <- newStdGen
-  let initialTile = (5.0, 5.0)
+  let initialTile = Tile {
+   pos = (5.0, 5.0)
+  }
   let initialBlock = Block {
     blockType = LBlock,
     rotation = 0,
     tiles = [initialTile]
   }
   let initialState = createNewBlock $ Game {
-      brickPos = initialBrickPos,
-      brickRotation = 0,
-      brickType = LineBlock,
+      currentBlock = initialBlock,
+      --brickPos = initialBrickPos,
+      --brickRotation = 0,
+      --brickType = LineBlock,
       keyPress = None,
       score = 0,
       gen = stdGen
@@ -70,20 +73,21 @@ randomRotation :: StdGen -> (Int, StdGen)
 randomRotation g = (90*r, g') where (r, g') = randomR (0,3) g
 
 render :: TetrisGame -> Picture 
-render g = pictures [translate x y $ rotate (brickRotation g) $ translate (-x) (-y) $ renderBrick g, renderDashboard g]
-  where (x, y) = brickPos g
+--render g = pictures [translate x y $ rotate (brickRotation g) $ translate (-x) (-y) $ renderBrick g, renderDashboard g]
+render g = pictures $ renderBrick g  -- renderDashboard g
+  --where (x, y) = brickPos g
 
 renderDashboard g = pictures [scorePic, nextBlockPic]
   where
-    scorePic = color white $ translate 100 50 $ scale 0.1 0.1 $ text $ "Score: " ++ (show $ score g)
-    nextBlockPic = color white $ translate 100 0 $ scale 0.1 0.1 $ text $ "Next block:" ++ (show $ brickRotation g)
+    scorePic     = color white $ translate 100 50 $ scale 0.1 0.1 $ text $ "Score: " ++ (show $ score g)
+    nextBlockPic = color white $ translate 100 0  $ scale 0.1 0.1 $ text $ "Next block:"
   
 
 renderBrick g
  = renderTiles (tiles (currentBlock g))
 
-renderTiles (t:ts)
-  = pictures [renderTile t] ++ renderTiles ts
+renderTiles []     = []
+renderTiles (t:ts) = renderTile t : renderTiles ts
 
 renderTile t = 
   translate x y $ rectangleSolid s s
@@ -99,35 +103,35 @@ renderTile t =
 -- | (brickType g) == ZBlock      = renderZBlock g
 
 -- TODO: separate block definition for render code (so can do collision detection in update)
-renderLineBlock g   = color blue $ pictures [renderBlock g 0 0, renderBlock g 1 0, renderBlock g 2 0, renderBlock g 3 0]
-renderLBlock g      = color (light blue) $ pictures [renderBlock g 0 0, renderBlock g 1 0, renderBlock g 2 0, renderBlock g 2 1]
-renderTBlock g      = color (light yellow) $ pictures [renderBlock g 0 0, renderBlock g 1 0, renderBlock g 2 0, renderBlock g 1 1]
-renderSquareBlock g = color rose $ pictures [renderBlock g 0 0, renderBlock g 1 0, renderBlock g 0 1, renderBlock g 1 1]
-renderSBlock g      = color green $ pictures [renderBlock g 0 0, renderBlock g (-1) 0, renderBlock g 0 1, renderBlock g 1 1]
-renderZBlock g      = color red $ pictures [renderBlock g 0 0, renderBlock g 1 0, renderBlock g 0 1, renderBlock g (-1) 1]
+--renderLineBlock g   = color blue $ pictures [renderBlock g 0 0, renderBlock g 1 0, renderBlock g 2 0, renderBlock g 3 0]
+--renderLBlock g      = color (light blue) $ pictures [renderBlock g 0 0, renderBlock g 1 0, renderBlock g 2 0, renderBlock g 2 1]
+--renderTBlock g      = color (light yellow) $ pictures [renderBlock g 0 0, renderBlock g 1 0, renderBlock g 2 0, renderBlock g 1 1]
+--renderSquareBlock g = color rose $ pictures [renderBlock g 0 0, renderBlock g 1 0, renderBlock g 0 1, renderBlock g 1 1]
+--renderSBlock g      = color green $ pictures [renderBlock g 0 0, renderBlock g (-1) 0, renderBlock g 0 1, renderBlock g 1 1]
+--renderZBlock g      = color red $ pictures [renderBlock g 0 0, renderBlock g 1 0, renderBlock g 0 1, renderBlock g (-1) 1]
 
-renderBlock g ox oy = 
-  translate x' y' $ rectangleSolid (tileSize-1) (tileSize-1)
-  where
-    (x, y) = brickPos g
-    x' = x + ox*tileSize
-    y' = y + oy*tileSize
+--renderBlock g ox oy = 
+--  translate x' y' $ rectangleSolid (tileSize-1) (tileSize-1)
+--  where
+--    (x, y) = brickPos g
+--    x' = x + ox*tileSize
+--    y' = y + oy*tileSize
 
 handleKeys :: Event -> TetrisGame -> TetrisGame
 handleKeys (EventKey (SpecialKey KeyLeft) Down _ _) g  = handleKeyPress $ g { keyPress = West }
 handleKeys (EventKey (SpecialKey KeyRight) Down _ _) g = handleKeyPress $ g { keyPress = East }
 handleKeys (EventKey (SpecialKey KeyDown) Down _ _) g  = handleKeyPress $ g { keyPress = South }
-handleKeys (EventKey (SpecialKey KeyUp) Down _ _) g    = g { brickRotation = ((brickRotation g) + 90) `mod'` 360 }
+handleKeys (EventKey (SpecialKey KeyUp) Down _ _) g    = g --{ brickRotation = ((brickRotation g) + 90) `mod'` 360 }
 handleKeys _ g = g { keyPress = None }
 
-moveBlock g (x', y')
-  | y <= -290 = createNewBlock g
-  | x <= -190 || x >= 190 = g
-  | otherwise = g { brickPos = (x, y) }
-  where (x, y) = brickPos g + (x', y')
+moveBlock g (x', y') = g
+--  | y <= -290 = createNewBlock g
+--  | x <= -190 || x >= 190 = g
+--  | otherwise = g --{ brickPos = (x, y) }
+--  where (x, y) = brickPos g + (x', y')
 
 createNewBlock g 
-  = g { brickPos = initialBrickPos, brickType = bt, brickRotation = fromIntegral br, gen = gen'' }
+  = g -- { brickPos = initialBrickPos, brickType = bt, brickRotation = fromIntegral br, gen = gen'' }
   where 
     (bt, gen')  = randomBlockType (gen g)
     (br, gen'') = randomRotation gen'
